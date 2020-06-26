17297e74989104981361c934eed5b2f969823711
Alexandre Brandão <alexandre.brandao.lustosa@gmail.com>
2020-06-26 20:22:11 -0300

    Ajustes

---
+++ kafka-producer-simples-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kafka_producer_simples_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
