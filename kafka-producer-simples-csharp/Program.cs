4d7d646425052df8eb40bc75dc4722ed23ea3d56
Alexandre Brandão <alexandre.brandao.lustosa@gmail.com>
2020-06-26 21:52:45 -0300

    First commit

--- kafka-producer-simples-csharp/Program.cs
+++ kafka-producer-simples-csharp/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace kafka_producer_simples_csharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var config = new ProducerConfig {
+                BootstrapServers = "omnibus-01.srvs.cloudkafka.com:9094",
+                SaslUsername = "f5xaw18s",
+                SaslPassword = "GDrH3mHRwasLltJ4O7JRde73sPUN0y6h",
+                SaslMechanism = SaslMechanism.Plain,
+                SecurityProtocol = SecurityProtocol.SaslPlaintext
+            };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            try
+            {
+                var count = 0;
+
+                while (true)
+                {
+                    var dr = await p.ProduceAsync("f5xaw18s-apptest", new Message<Null, string> { Value = $"test: {count}" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}' -> count: {count}");
+
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
         }
     }
 }
