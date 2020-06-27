ba8fcda883ade850a90995652fbac7fc4f43e604
Alexandre Brandão <alexandre.brandao.lustosa@gmail.com>
2020-06-27 20:00:27 -0300

    Ajustes

--- kafka-producer-simples-csharp/Program.cs
+++ kafka-producer-simples-csharp/Program.cs
@@ -7,29 +7,30 @@ namespace kafka_producer_simples_csharp
 {
     class Program
     {
-        static async Task Main()
+        public static async Task Main()
         {
-            var config = new ProducerConfig {
-                BootstrapServers = "localhost:9092"
-            };
+            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
 
             using var p = new ProducerBuilder<Null, string>(config).Build();
 
-            try
             {
-                var count = 0;
-
-                while (true)
+                try
                 {
-                    var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = $"test: {count}" });
-                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}' -> count: {count}");
+                    var count = 0;
+                    while (true)
+                    {
+                        var dr = await p.ProduceAsync("test-topic", 
+                            new Message<Null, string> { Value = $"test: {count++}" });
 
-                    Thread.Sleep(2000);
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+
+                        Thread.Sleep(2000);
+                    }
                 }
-            }
-            catch (ProduceException<Null, string> e)
-            {
-                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
             }
         }
     }
